36f5b11c5a40f53427053e170e52ae3347124088
leoli <chichali254@gmail.com>
2026-01-08 00:03:28 +0800

    fix: 修補swiper導致跑版的問題

--- src/pages/subscription/Detail.jsx
+++ src/pages/subscription/Detail.jsx
@@ -13,133 +13,135 @@ export default function Detail() {
   return(
     <div>
       <section className="bg-gray-98 py-md-25 py-20">
-      <div className="container text-center"> 
-      <h1 className="fs-md-5xl fs-4 pb-6 border-bottom mb-6">選擇適合你的使用方案</h1>
-        <div className="mb-6">
-          <span className="fs-md-2xl fs-s badge text-tag-secondary bg-tag-secondary-bg py-md-2 px-md-4 py-1 px-2 me-6 rounded-1"># 簡單起步</span>
-          <span className="fs-md-2xl fs-s badge text-tag-orange bg-tag-orange-bg py-md-2 px-md-4 py-1 px-2 me-6 rounded-1"># 彈性升級</span>
-          <span className="fs-md-2xl fs-s badge text-tag-success bg-tag-success-bg py-md-2 px-md-4 py-1 px-2 rounded-1"># 讓筆記陪你成長</span>
-        </div>
-        <p className="fs-md-2xl fs-l fw-bold mb-md-20 mb-10 d-flex flex-column flex-md-row justify-content-center align-items-center text-center" >無論你是學生還是專業人士，<br className="d-md-none"/>我們都有適合你的方案。</p>     
-       </div> 
-      <Swiper className="plan-swiper" initialSlide={2} centeredSlides={true} slidesPerView={"auto"} spaceBetween={40} centerInsufficientSlides={true} centeredSlidesBounds={true} resistanceRatio={0} watchOverflow={true} breakpoints={{320: {
-             spaceBetween: 24,
-          },
-          992: {
-            spaceBetween: 40
-           }}}>
+        <div className="container text-center"> 
+          <h1 className="fs-md-5xl fs-4 pb-6 border-bottom mb-6">選擇適合你的使用方案</h1>
+          <div className="mb-6">
+            <span className="fs-md-2xl fs-s badge text-tag-secondary bg-tag-secondary-bg py-md-2 px-md-4 py-1 px-2 me-6 rounded-1"># 簡單起步</span>
+            <span className="fs-md-2xl fs-s badge text-tag-orange bg-tag-orange-bg py-md-2 px-md-4 py-1 px-2 me-6 rounded-1"># 彈性升級</span>
+            <span className="fs-md-2xl fs-s badge text-tag-success bg-tag-success-bg py-md-2 px-md-4 py-1 px-2 rounded-1"># 讓筆記陪你成長</span>
+          </div>
+          <p className="fs-md-2xl fs-l fw-bold mb-md-20 mb-10 d-flex flex-column flex-md-row justify-content-center align-items-center text-center" >無論你是學生還是專業人士，<br className="d-md-none"/>我們都有適合你的方案。</p>     
+        </div> 
+        <div style={{ width: '100%', overflow: 'hidden' }}>
+        <Swiper style={{ overflow: 'visible' }} className="plan-swiper" initialSlide={2} centeredSlides={true} slidesPerView={"auto"} spaceBetween={40} centerInsufficientSlides={true} centeredSlidesBounds={true} resistanceRatio={0} watchOverflow={true} breakpoints={{320: {
+              spaceBetween: 24,
+            },
+            992: {
+              spaceBetween: 40
+            }}}>
 
-            {/* Slide 1 */}
-           <SwiperSlide className="col-md-4 col-12 h-auto">
-             <div className="bg-gray-0 p-6 border border-gray-100 rounded-4 d-flex flex-column h-100">
-               <div>
-                 <div className="pb-6 border-bottom border-gray-400 mb-8">
-                   <div className="d-flex justify-content-between mb-6">
-                     <span className="badge bg-secondary text-gray-0 border border-gray-500 px-md-4 fs-s fs-md-l">Free</span>
-                     <span className="fs-s fs-md-l fw-bold text-gray-500">免費方案</span>
-                   </div>
-                   <h3 className="text-start">
-                     <span className="text-gray-1000 fs-4xl fs-md-5xl">NT$ 0</span>
-                     <span className="text-gray-500 lh-base fw-normal fs-m fs-md-xl">/ 月</span>
-                   </h3>
-                 </div>
-                 <ul className="list-unstyled mb-8 text-start">
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">建立筆記卡片</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">建立書單</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">標籤分類</span>
-                   </li>
-                 </ul>
-               </div>
-               <a href="#" className="btn btn-outline-gray-400 py-md-4 mt-auto w-100 fs-md-5 fs-6">立即註冊</a>
-             </div>
-           </SwiperSlide>
-           <SwiperSlide className="col-md-4 col-12 h-auto">
-             <div className="bg-gray-0 p-6 border border-2 border-secondary rounded-4 shadow d-flex flex-column h-100">
-               <div>
-                 <div className="pb-6 border-bottom border-gray-400 mb-8">
-                   <div className="d-flex justify-content-between mb-6">
-                     <span className="badge bg-secondary text-gray-0 border border-gray-500 px-md-4 fs-s fs-md-l">Pro</span>
-                     <span className="fs-s fs-md-l fw-bold text-gray-500">月繳方案</span>
-                   </div>
-                   <h3 className="text-start">
-                     <span className="text-gray-1000 fs-4xl fs-md-5xl">NT$ 120</span>
-                     <span className="text-gray-500 lh-base fw-normal fs-m fs-md-xl">/ 月</span>
-                   </h3>
-                 </div>
-                 <ul className="list-unstyled mb-8 text-start">
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">無限制建立筆記卡片</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">無限制建立書單</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">標籤分類</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">視覺化關聯地圖</span>
-                   </li>
-                   <li>
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">進度追蹤與提醒</span>
-                   </li>
-                 </ul>
-               </div>
-               <a href="#" className="btn btn-secondary py-md-4 mt-auto w-100 fs-md-5 fs-6">升級方案（推薦）</a>
-             </div>
-           </SwiperSlide>
-           <SwiperSlide className="col-md-4 col-12 h-auto">
-             <div className="bg-gray-0 p-6 border border-gray-100 rounded-4 d-flex flex-column h-100">
-               <div>
-                 <div className="pb-6 border-bottom border-gray-400 mb-8">
-                   <div className="d-flex justify-content-between mb-6">
-                     <span className="badge bg-secondary text-gray-0 border border-gray-500 px-md-4 fs-s fs-md-l">Pro</span>
-                     <span className="fs-s fs-md-l fw-bold text-gray-500">年繳方案</span>
-                   </div>
-                   <h3 className="text-start">
-                     <span className="text-gray-1000 fs-4xl fs-md-5xl">NT$ 1200</span>
-                     <span className="text-gray-500 lh-base fw-normal fs-m fs-md-xl">/ 年</span>
-                   </h3>
-                 </div>
-                 <ul className="list-unstyled mb-8 text-start">
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">無限制建立筆記卡片</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">無限制建立書單</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">標籤分類</span>
-                   </li>
-                   <li className="mb-3">
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">視覺化關聯地圖</span>
-                   </li>
-                   <li>
-                     <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
-                     <span className="text-gray-1000">進度追蹤與提醒</span>
-                   </li>
-                 </ul>
-               </div>
-               <a href="#" className="btn btn-outline-gray-400 py-md-4 mt-auto w-100 fs-md-5 fs-6">升級方案</a>
-             </div>
-           </SwiperSlide> 
-     </Swiper>
+              {/* Slide 1 */}
+            <SwiperSlide className="col-md-4 col-12 h-auto">
+              <div className="bg-gray-0 p-6 border border-gray-100 rounded-4 d-flex flex-column h-100">
+                <div>
+                  <div className="pb-6 border-bottom border-gray-400 mb-8">
+                    <div className="d-flex justify-content-between mb-6">
+                      <span className="badge bg-secondary text-gray-0 border border-gray-500 px-md-4 fs-s fs-md-l">Free</span>
+                      <span className="fs-s fs-md-l fw-bold text-gray-500">免費方案</span>
+                    </div>
+                    <h3 className="text-start">
+                      <span className="text-gray-1000 fs-4xl fs-md-5xl">NT$ 0</span>
+                      <span className="text-gray-500 lh-base fw-normal fs-m fs-md-xl">/ 月</span>
+                    </h3>
+                  </div>
+                  <ul className="list-unstyled mb-8 text-start">
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">建立筆記卡片</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">建立書單</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">標籤分類</span>
+                    </li>
+                  </ul>
+                </div>
+                <a href="#" className="btn btn-outline-gray-400 py-md-4 mt-auto w-100 fs-md-5 fs-6">立即註冊</a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="col-md-4 col-12 h-auto">
+              <div className="bg-gray-0 p-6 border border-2 border-secondary rounded-4 shadow d-flex flex-column h-100">
+                <div>
+                  <div className="pb-6 border-bottom border-gray-400 mb-8">
+                    <div className="d-flex justify-content-between mb-6">
+                      <span className="badge bg-secondary text-gray-0 border border-gray-500 px-md-4 fs-s fs-md-l">Pro</span>
+                      <span className="fs-s fs-md-l fw-bold text-gray-500">月繳方案</span>
+                    </div>
+                    <h3 className="text-start">
+                      <span className="text-gray-1000 fs-4xl fs-md-5xl">NT$ 120</span>
+                      <span className="text-gray-500 lh-base fw-normal fs-m fs-md-xl">/ 月</span>
+                    </h3>
+                  </div>
+                  <ul className="list-unstyled mb-8 text-start">
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">無限制建立筆記卡片</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">無限制建立書單</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">標籤分類</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">視覺化關聯地圖</span>
+                    </li>
+                    <li>
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">進度追蹤與提醒</span>
+                    </li>
+                  </ul>
+                </div>
+                <a href="#" className="btn btn-secondary py-md-4 mt-auto w-100 fs-md-5 fs-6">升級方案（推薦）</a>
+              </div>
+            </SwiperSlide>
+            <SwiperSlide className="col-md-4 col-12 h-auto">
+              <div className="bg-gray-0 p-6 border border-gray-100 rounded-4 d-flex flex-column h-100">
+                <div>
+                  <div className="pb-6 border-bottom border-gray-400 mb-8">
+                    <div className="d-flex justify-content-between mb-6">
+                      <span className="badge bg-secondary text-gray-0 border border-gray-500 px-md-4 fs-s fs-md-l">Pro</span>
+                      <span className="fs-s fs-md-l fw-bold text-gray-500">年繳方案</span>
+                    </div>
+                    <h3 className="text-start">
+                      <span className="text-gray-1000 fs-4xl fs-md-5xl">NT$ 1200</span>
+                      <span className="text-gray-500 lh-base fw-normal fs-m fs-md-xl">/ 年</span>
+                    </h3>
+                  </div>
+                  <ul className="list-unstyled mb-8 text-start">
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">無限制建立筆記卡片</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">無限制建立書單</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">標籤分類</span>
+                    </li>
+                    <li className="mb-3">
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">視覺化關聯地圖</span>
+                    </li>
+                    <li>
+                      <span className="material-symbols-outlined align-bottom text-secondary me-4"> check </span>
+                      <span className="text-gray-1000">進度追蹤與提醒</span>
+                    </li>
+                  </ul>
+                </div>
+                <a href="#" className="btn btn-outline-gray-400 py-md-4 mt-auto w-100 fs-md-5 fs-6">升級方案</a>
+              </div>
+            </SwiperSlide> 
+        </Swiper>
+        </div>
      </section>
     <section className="faq py-md-25 py-20">
       <div className="faq-area">
