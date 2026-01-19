83b5a4f6a3ea77e7461664f75af0435cdf0ba7e6
tanukili <108317269+tanukili@users.noreply.github.com>
2026-01-19 21:28:28 +0800

    fix: 刪除不必要的 css 設定與檔案

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "material-symbols";
 import "./assets/index.scss";
-// import 'bootstrap/dist/js/bootstrap.min.js';
+
 import { Provider } from "react-redux";
 import { store } from "./store/index";
 
