9e2891d8ffae74858db859464e2a954bf7054f7c
tanukili <108317269+tanukili@users.noreply.github.com>
2026-02-03 09:14:29 +0800

    feat: 卡片盒詳細 modal、基礎卡片切版

--- src/components/card/BaseCard.jsx
+++ src/components/card/BaseCard.jsx
@@ -1,24 +1,52 @@
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { prism } from "react-syntax-highlighter/dist/esm/styles/prism";
 
-export default function BaseCard({ card, badges }) {
-  const findBadge = (badgeId) => badges.find((b) => b.id === badgeId);
+export default function BaseCard({ card, badges, mode = "base" }) {
+  // 卡片三種顯示模式：base、titleOnly、withBadge
+  const findBadge = (badgeId) => badges?.find((b) => b.id === badgeId);
+
+  const cardStyle = {
+    base: "card rounded-4 bg-gray-0 border-gray-500 mb-4",
+    titleOnly: "card rounded-4 bg-gray-0 border-gray-200 mb-4",
+    withBadge: "card rounded-4 bg-gray-0 border-gray-200 mb-4",
+  };
+
+  const brandTheme = {
+    'code[class*="language-"]': {
+      color: "var(--bs-gray-900)",
+    },
+    keyword: {
+      color: "var(--bs-primary)",
+    },
+    function: {
+      color: "var(--bs-secondary)",
+    },
+    string: {
+      color: "var(--bs-tag-success)",
+    },
+    number: {
+      color: "var(--bs-primary-400)",
+    },
+    punctuation: { color: "var(--bs-gray-500)" },
+  };
 
   return (
-    <div className="card rounded-4 bg-gray-0 border-gray-200 mb-4">
+    <div className={cardStyle[mode]}>
+      <div className="card-header">
+        <h3 className="fs-m text-center lh-base tracking-2">{card.title}</h3>
+      </div>
       <div className="card-body p-lg-6">
-        <span
+        {/* <span
           className={`badge badge-${findBadge(card.badgeId).ui} lh-base mb-4`}
         >
           {findBadge(card.badgeId).text}
-        </span>
-        <ReactMarkdown>{card.title}</ReactMarkdown>
+        </span> */}
+        {/* <ReactMarkdown>{card.title}</ReactMarkdown> */}
         <ReactMarkdown
           // 客製化樣式
           components={{
-            // node 該元素在語法樹中的原始資料物件；props 物件，此 HTML 標籤應該要有的所有屬性
-            ul: ({ node, ...props }) => <ul className="mb-0" {...props} />,
+            // node 是該元素在語法樹中的原始資料物件；props 物件是此 HTML 標籤應該要有的所有屬性
+            ul: ({ node, ...props }) => <ul className="list-unstyled mb-0 ps-6" {...props} />,
             hr: ({ node }) => <hr className="my-3" />,
             // 取消外層預設的 pre 標籤
             pre: ({ children }) => <>{children}</>,
@@ -27,11 +55,19 @@ export default function BaseCard({ card, badges }) {
 
               return language ? (
                 <SyntaxHighlighter
-                  style={prism}
+                  style={brandTheme}
+                  customStyle={{
+                    backgroundColor: "var(--bs-gray-50)",
+                    borderRadius: "var(--bs-border-radius-sm)",
+                    padding: ".75rem",
+                    fontWeight: "500",
+                    marginTop: "1rem",
+                  }}
                   language={language}
                   PreTag="pre"
                   codeTagProps={{
                     style: {
+                      color: "var(--bs-gray-900)",
                       whiteSpace: "pre-wrap",
                       wordBreak: "break-all",
                     },
